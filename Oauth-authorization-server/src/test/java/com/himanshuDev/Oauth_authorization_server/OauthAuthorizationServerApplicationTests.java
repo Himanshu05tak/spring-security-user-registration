d37b9066bacbcaf85d523dff6adcd0e4cbe1055b
Himanshu05Tak <himanshu05.tak@gmail.com>
2026-08-24 23:19:53 +0200

    feat: Add OAuth Authorization Server
    Changes
    
     - Configure the OAuth module to inherit from the parent POM
     - Add the Spring Security dependency to the OAuth module
     - Set up the foundation for OAuth 2.0 Authorization Server functionalities

---
+++ Oauth-authorization-server/src/test/java/com/himanshuDev/Oauth_authorization_server/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshuDev.Oauth_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
